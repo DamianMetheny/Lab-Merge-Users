825e22caf9962e5c21ca29acdc40a7aa8e380bb7
Damian Metheny <metheny555@outlook.com>
2025-05-19 22:48:01 -0400

    Damian's Modern JS lab

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
+const datejs = require('datejs');
 
+function combineUsers(...args) {
+  const combinedObject = {
+    users: [],
+    merge_date: ''
+  };
+
+  for (const userArray of args) {
+    combinedObject.users = [...combinedObject.users, ...userArray];
+  }
+
+  combinedObject.merge_date = new Date().toString('M/d/yyyy');
+
+  return combinedObject;
+}
 
 
 module.exports = {
